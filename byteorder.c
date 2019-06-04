7389e7ed056a89a437df037e945499ddac7d2e92
mankitty <39092080+mankitty@users.noreply.github.com>
2019-06-04 14:46:18 +0800

    Update byteorder.c

--- byteorder.c
+++ byteorder.c
@@ -9,7 +9,8 @@ int main(int argc,char **argv)
 	
 	un.s = 0x0102;
 	
-	if (sizeof(short) == 2) {
+	if (sizeof(short) == 2) 
+        {
 		if (un.c[0] == 0x1 && un.c[1] == 0x2) {
 			printf("big-endian\n");
 		} else if (un.c[0] == 0x2 && un.c[1] == 0x1){
@@ -21,3 +22,23 @@ int main(int argc,char **argv)
 		printf("sizeof(short):%d\n",sizeof(short));
 	}
 }
+
+
+#include <stdio.h>
+ 
+#include <sys/types>
+ 
+main()
+ 
+{
+ 
+int x=0x12345678; /* 305419896 */
+ 
+unsigned char *p=(char *)&x;
+ 
+printf("%0x % 0x %0x %0x",xp[0],xp[1],xp[2],xp[3]);
+ 
+}
+输出：78 56 34 12
+
+说明是小端！反过来的则是大端
